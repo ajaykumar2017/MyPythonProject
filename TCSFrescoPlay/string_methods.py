2237695df12dfd8b22a31eaeeb3af059f629de5f
Ajay Kumar <ajaykumar201761@gmail.com>
2020-07-17 17:57:31 +0530

    Solved upto Section 4

--- TCSFrescoPlay/string_methods.py
+++ TCSFrescoPlay/string_methods.py
@@ -7,61 +7,62 @@ import re
 import sys
 
 
-#
-# Complete the 'strmethod' function below.
-#
-# The function accepts following parameters:
-#  1. STRING para
-#  2. STRING spch1
-#  3. STRING spch2
-#  4. LIST li1
-#  5. STRING strf
-#
-
-def stringmethod(para, special1, special2, list1, strfind):
-    # Write your code here
+def stringMethod(para, special1, special2, list1, strfind):
+    # Section 1 --> Ok
     word1 = ""
     for character in para:
         if character not in special1:
             word1 = word1 + character
-
     rword2 = word1[0:70]
-    print(rword2)
+    print(rword2[::-1])
 
-    rword2 = rword2.strip()
-    print(rword2 + special2)
+    # Section 2 --> Ok
+    rword3 = rword2[::-1]
+    rword3 = re.sub("\s+", "", rword3)
+    temp = ""
+    for character in rword3:
+        temp = temp + character + special2
+    print(temp[:-1])
 
+    # Section 3 --> Ok
     a = True
     for s in list1:
         if s not in para:
             a = False
-
     if a:
-        print("Every string in {list1} were present")
+        print("Every string in", end=" "),
+        print(list1, end=" "),
+        print("were present")
     else:
-        print("Every string in {list1} were not present")
+        print("Every string in", end=" "),
+        print(list1, end=" "),
+        print("were not present")
 
-    print(word1.split(' ', 20))
-    Counter = Counter(word1.split())
-    print(Counter.less_common(20))
+    # Section 4 --> OK
+    print(word1.split()[:20])
+
+    # Section 5
+    word_list = word1.split()
+
+    temp_list = []
+    for str1 in word_list:
+        if word_list.count(str1) <= 3:
+            temp_list.append(str1)
+
+    print(temp_list[:20])
+
+    # Section 6
     print(word1.rindex(strfind))
 
 
 if __name__ == '__main__':
-    para = input()
-
+    para = input()  # a string
     spch1 = input()
-
     spch2 = input()
-
     qw1_count = int(input().strip())
-
     qw1 = []
-
     for _ in range(qw1_count):
         qw1_item = input()
         qw1.append(qw1_item)
-
     strf = input()
-
-    stringmethod(para, spch1, spch2, qw1, strf)
+    stringMethod(para, spch1, spch2, qw1, strf)
