6dee54edef3e0f578299b8e616701cc43802e5b9
Ajay Kumar <ajaykumar201761@gmail.com>
2020-07-17 19:22:20 +0530

    refractor

--- TCSFrescoPlay/Python3FunctionsandOOPs/magicConstantGenerator.py
+++ TCSFrescoPlay/Python3FunctionsandOOPs/magicConstantGenerator.py
@@ -1,25 +1,7 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-#
-# Complete the 'Magic_const' function below.
-#
-#
-#
-# The function accepts INTEGER n1 as parameter.
-#
-
 def generator_Magic(n1):
-
     for test in range(3, n1 + 1):
         yield (test * (pow(test, 2) + 1)) / 2
-# Write your code here
+
 
 if __name__ == '__main__':
 
